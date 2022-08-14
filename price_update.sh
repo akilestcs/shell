85bfd0c92ff97485cf4140a9c4d9321597bac145
akilestcs <79924781+akilestcs@users.noreply.github.com>
2022-08-14 20:42:20 +0530

    Update price_update.sh

--- price_update.sh
+++ price_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#script to update all the decimal value of the product to .99
+#script to update all the decimal value of the product to .99 in Woocommerce
 exec >/tmp/"$(basename "$0").log" 2>&1 #log;
 mysql -uUSERNAME -pPASSWORD DBNAME <<EOF
 update wp_postmeta set meta_value = CEILING(meta_value)-0.01 where meta_key in ('_price','_regular_price') and meta_value<>'';
